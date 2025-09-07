4c7fbd197981ef7f03b1486b42e907259318765c
Rudranarayan Padhan <161827362+Rudra9Padhan@users.noreply.github.com>
2025-09-07 14:52:39 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
 import { placeOrder, verifyOrder, userOrders, listOrders, updateStatus, cancelOrder } from "../controllers/orderController.js"; // Import cancelOrder
-
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
